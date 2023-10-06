029a403c0929f1c0b512d456d5d5db7828d689e6
gregglrnt <gregoryslaurent@gmail.com>
2023-10-06 13:24:07 +0200

    Update enemy script

--- scripts/EnemyFollow.gd
+++ scripts/EnemyFollow.gd
@@ -2,10 +2,13 @@ extends Path3D
 
 #TODO: make this script generic
 
-@export var pathFollow: PathFollow3D
+var pathFollow : PathFollow3D;
+var mesh : Node3D;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pathFollow = self.get_child(0, true);
+	mesh = pathFollow.get_child(0, true);
 	pass # Replace with function body.
 
 
@@ -15,9 +18,9 @@ func _process(delta):
 	
 func _physics_process(delta):
 	const move_speed := 0.1	
-	if($Enemy1PathFollow.progress_ratio <= 1):
+	if(pathFollow.progress_ratio <= 1):
 		pathFollow.progress += move_speed + delta
-		$Enemy1PathFollow/Enemy1/Enemy.isWalking = true;
+		mesh.get_child(0).isWalking = true;
 	else: 
-		$Enemy1PathFollow/Enemy1/Enemy.isWalking = false
+		mesh.get_child(0).isWalking = false
 	
